78a97b54be98099e23cf5df8f72193eb9868f2fe
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2022-10-29 22:17:03 -0300

    dawd

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
 let lat = 0;
 let long = 0;
 
-app.listen(3000, '192.168.59.49', () => {
-    console.log("Started in port 3000")
+app.listen(8080, () => {
+    console.log("Started in port 8080")
 })
 
 app.get('/getGreeting', function (req, res) {
