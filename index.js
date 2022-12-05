881af73051943fb8cf8524d1c921b854b2a773f3
ricardolopezb <ricardolopezbonaguro@gmail.com>
2022-12-05 13:18:39 -0300

    fixed datetime timezone

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/location', (req, res) => {
 const setDateTime = () => {
     const date = new Date()
     return date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + 
-            " - " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+            " - " + (date.getHours()-3) + ":" + date.getMinutes() + ":" + date.getSeconds()
 
 }
 
